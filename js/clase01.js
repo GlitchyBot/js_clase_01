6ab32ff219fa8c6c32a7fe43ca21877bad4a32ba
Geronimo Bonzon <geronimobonzon@gmail.com>
2020-08-23 15:45:15 -0300

    Primera clase

---
+++ js/clase01.js
@@ -0,0 +1,11 @@
+var saludo = "Hola";
+var numeroA = 17;
+nombre = prompt('Ingrese su nombre, por favor:');
+numeroB = prompt('Ingrese un numero, por favor:');
+numeroB = parseInt(numeroB);
+suma = numeroA + numeroB;
+
+alert('Bienvenido, '+ nombre + '!');
+
+console.log('Primera clase');
+console.info('Prueba.');
